1933523975231345175d6ee940eb95ca35f76b63
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-02-23 13:12:49 +0800

    added input accepting

--- SALACUT_MP2.py
+++ SALACUT_MP2.py
@@ -1,1 +1,14 @@
-print("Hello Cruel World")
+
+def Language_Checker(Expression):
+    for x in range(0, int(input())):
+        print(Expression)
+    
+    return "Fuck"
+
+
+def main():
+    for x in range(0, int(input())):
+        result = Language_Checker(input())
+
+
+main()
