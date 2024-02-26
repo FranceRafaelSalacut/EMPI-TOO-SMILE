b355eb827e014e1bbc89f0696ff298838e767ed0
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-02-26 21:26:36 +0800

    something

--- SALACUT_MP2.py
+++ SALACUT_MP2.py
@@ -3,11 +3,18 @@ import re as regex
 def Language_Checker(Expression):
     Expression = Expression.replace(" ","")
     Expression = Expression.replace("+","|")
-    subjects = []
+    if Expression == "e":
+        Expression == ""
     #ic()
     for x in range(0, int(input())):
         #ic()
-        if regex.fullmatch(Expression, input()):
+
+        test_case = input()
+
+        if test_case == "e":
+            test_case = ""
+
+        if regex.fullmatch(Expression, test_case):
             print("yes")
         else:
             print("no")
