1a934ec13967919b8250d911107d63a5dfa38210
FranceRafaelSalacut <fpsalacut@up.edu.ph>
2024-02-27 01:20:29 +0800

    This code passes all test

--- SALACUT_MP2.py
+++ SALACUT_MP2.py
@@ -1,14 +1,16 @@
+from icecream import ic
 import re as regex
 
 def Language_Checker(Expression):
-    Expression = Expression.replace(" ","")
-    Expression = Expression.replace("+","|")
-    if Expression == "e":
-        Expression == ""
-    #ic()
+    Expression = Expression.replace(" ","").replace("+","|")
+    if Expression == 'e':
+        Expression = ""
+    if "e" in Expression: 
+        ic()
+        Expression = Expression.replace("e","")
+    ic(Expression)
     for x in range(0, int(input())):
         #ic()
-
         test_case = input()
 
         if test_case == "e":
